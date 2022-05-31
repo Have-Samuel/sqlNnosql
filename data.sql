07894a526725b95b96db44a25930091c29b8b272
Have Samuel <samiehave9@gmail.com>
2022-05-31 15:56:22 +0300

    Added data to the data file

--- data.sql
+++ data.sql
@@ -1,5 +1,13 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (1, 'Ramesh', 32, 'Ahmedabad', 2000.00 );
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, number_of_escapad)
+VALUES ('Agumon' 2020-02-03, 10.23, yes, 2);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, number_of_escapad)
+VALUES ('Gabumon', 2018-11-15, 8 no, );
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, number_of_escapad)
+VALUES ('Pikachu', 2021-01-07, 15.04, no, 1 );
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, number_of_escapad)
+VALUES ('Devimon', 2017-05-12, 11, yes, 5);
