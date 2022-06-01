ad8c17acf3c679018b17b3bf0085f0c0969ab750
Have Samuel <samiehave9@gmail.com>
2022-06-01 20:12:53 +0300

    Made changes

--- data.sql
+++ data.sql
@@ -22,37 +22,3 @@ INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attemp
 VALUES (10, 'Blossom', '1998-09-13', 17, TRUE, 3);
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES (11, 'Ditto', '2022-05-14', 22, TRUE, 4);
-
-/*Transaction*/
-/*Add 'Unspecified' as Species for each animal in the entire table then ROLLBACK the change*/
-BEGIN;
-UPDATE animals SET species = 'unspecified'
-ROLLBACK;
-
-/*Set Species to DIGIMON and POKEMON for all named animals with 'mon' in the name. Then sets Species to POKEMON for each remaining animal without a special sset. 
-This is a commit Transaction [permanentchange]*/
-BEGIN;
-UPDATE animals SET species = 'DIGIMON' WHERE name like '%mon%';
-UPDATE animals SET species = 'POKEMON' WHERE species is NULL;
-COMMIT;
-
-/* Delete All records in the database, then rollback the delete to bring all of the data*/
-BEGIN;
-DELETE FROM animals;
-ROLLBACK;
-
-/*Deletes all animals born after January 1st 2022, 
-makes a savepoint after the deletion,
-updates all animals to the product of their weight_kg * -1,
-rolls-back to the previous savepoint,
-multiplies the weight of ONLY animals with negative weight_kg to the product of weightKG * -1.
-This is a COMMIT Transaction (permanent change).
-RESULT: All remaining animals have positive weight values*/
-
-BEGIN;
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-SAVEPOINT SP1;
-UPDATE animals SET weight_kg = weight_kg * -1;
-ROLLBACK TO SP1;
-UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
-COMMIT;
