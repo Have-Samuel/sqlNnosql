d9bfc4ec35852b62a2d45a87fca0c8d2dc8f411b
Have Samuel <samiehave9@gmail.com>
2022-06-02 14:26:44 +0300

    A little changes

--- data.sql
+++ data.sql
@@ -1,25 +1,25 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES (1, 'Agumon', '2020-02-03', 10.23, TRUE, 0);
+VALUES ('Agumon', '2020-02-03', 10.23, TRUE, 0);
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES (2, 'Gabumon', '2018-11-15', 8, FALSE, 2);
+VALUES ('Gabumon', '2018-11-15', 8, FALSE, 2);
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES (3, 'Pikachu', '2021-01-07', 15.04, FALSE, 1 );
+VALUES ('Pikachu', '2021-01-07', 15.04, FALSE, 1 );
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES (4, 'Devimon', '2017-05-12', 11, TRUE, 5);
+VALUES ('Devimon', '2017-05-12', 11, TRUE, 5);
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES (5, 'Charmander', '2020-02-08', -11, FALSE, 0);
+VALUES ('Charmander', '2020-02-08', -11, FALSE, 0);
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES (6, 'Plantmon', '2021-11-15', -5.7, TRUE, 2);
+VALUES ('Plantmon', '2021-11-15', -5.7, TRUE, 2);
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES (7, 'Squirtle', '1993-04-02', -12.13, FALSE, 3);
+VALUES ('Squirtle', '1993-04-02', -12.13, FALSE, 3);
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES (8, 'Angemon', '2005-06-12', -45, TRUE, 1);
+VALUES ('Angemon', '2005-06-12', -45, TRUE, 1);
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES (9, 'Boarmon', '2005-06-07', 20.4, TRUE, 7);
+VALUES ('Boarmon', '2005-06-07', 20.4, TRUE, 7);
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES (10, 'Blossom', '1998-09-13', 17, TRUE, 3);
+VALUES ('Blossom', '1998-09-13', 17, TRUE, 3);
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES (11, 'Ditto', '2022-05-14', 22, TRUE, 4);
 
@@ -28,3 +28,8 @@ VALUES(12, 'Sam Smith', 34), (12, 'Jenifer Orwell', 19), (12, 'Bob', 45), (12, '
 INSERT INTO species(id, name)
 VALUES(13,  'Pokemon',), (13, 'Digimon')
 
+UPDATE animals SET owner_id = 1 WHERE name LIKE 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
